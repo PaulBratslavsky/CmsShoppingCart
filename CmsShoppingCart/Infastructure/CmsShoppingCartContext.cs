9c0450d528fb1ee82842e94ddfccaf59f24df7fe
paulbratslavsky <paul.bratslavsky@gmail.com>
2020-06-25 21:10:25 -0700

    database

---
+++ CmsShoppingCart/Infastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
