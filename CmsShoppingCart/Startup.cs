ca0f92eadc13f81ba88161419a6a974aa2213ddc
paulbratslavsky <paul.bratslavsky@gmail.com>
2020-06-26 12:32:56 -0700

    Added admin page and view

--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -53,6 +53,10 @@ namespace CmsShoppingCart
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
